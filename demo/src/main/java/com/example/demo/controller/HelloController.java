0efbaab5d8d074501c12f1a9c8992726fda66e36
Dsik93 <116441206+Dsik93@users.noreply.github.com>
2025-01-11 17:46:48 +0100

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demoWeb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot 3.3.x!";
+    }
+}
